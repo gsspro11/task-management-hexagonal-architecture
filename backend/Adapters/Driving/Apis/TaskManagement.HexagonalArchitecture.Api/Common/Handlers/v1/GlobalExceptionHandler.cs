c7a9934bdaf5c86ef55baeac7a49b06e24a7bc9e
Guilherme Santos <guilherme.santos1@edenred.com>
2025-11-27 18:06:10 -0300

    Refactor `GlobalExceptionHandler` to use a consistent `ILogger` variable name.

--- backend/Adapters/Driving/Apis/TaskManagement.HexagonalArchitecture.Api/Common/Handlers/v1/GlobalExceptionHandler.cs
+++ backend/Adapters/Driving/Apis/TaskManagement.HexagonalArchitecture.Api/Common/Handlers/v1/GlobalExceptionHandler.cs
@@ -3,14 +3,14 @@ using TaskManagement.HexagonalArchitecture.Domain.Abstractions;
 
 namespace TaskManagement.HexagonalArchitecture.Api.Common.Handlers.v1
 {
-    internal sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> _logger) : IExceptionHandler
+    internal sealed class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
     {
         public async ValueTask<bool> TryHandleAsync(
             HttpContext httpContext,
             Exception exception,
             CancellationToken cancellationToken)
         {
-            _logger.LogError(
+            logger.LogError(
                 exception, "Exception occurred: {Message}", exception.Message);
 
             var problemDetails = new CustomError[] { new(exception.GetType().Name, exception.Message) };
